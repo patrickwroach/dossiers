a6435d99aa7779f90c4e8a9279fde673919529f8
proach720 <proach720@comcast.com>
2021-01-22 11:00:28 -0500

    setting up state for new friend

--- src/firebase/firebaseActions.js
+++ src/firebase/firebaseActions.js
@@ -1,41 +1,49 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 import firebaseConfig from './firebaseConfig';
 
 firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
 
- const addFriend = (firstName, lastName) => {
-    const docId =`${lastName}-${firstName}`
-    var friendsRef = db.collection("friends");
-    var docRef = friendsRef.doc(docId);
-    docRef.get().then(function(doc) {
-        if (doc.exists) {
-            alert("already have this friend");
-        } else {
-          friendsRef.doc(docId).set({
-            firstName: firstName,
-            lastName: lastName,
-          });
-        }
-    }).catch(function(error) {
-        console.log("Error getting document:", error);
+const addFriend = (firstName, lastName) => {
+  const docId = `${lastName}-${firstName}`;
+  var friendsRef = db.collection('friends');
+  var docRef = friendsRef.doc(docId);
+  docRef.get().then(function (doc) {
+    if (doc.exists) {
+      alert('already have this friend');
+    } else {
+      friendsRef.doc(docId).set({
+        firstName: firstName,
+        lastName: lastName,
+      });
+    }
+  }).catch(function (error) {
+    console.log('Error getting document:', error);
+  });
+};
+
+const getFriends = () => {
+  let allFriends = [];
+  db.collection('friends').get().then(function (querySnapshot) {
+    
+    querySnapshot.forEach(function (doc) {
+      let aFriend = doc.data();
+      aFriend.id = doc.id;
+      allFriends.push(aFriend);
+      
     });
-  }
+    
+  });
+  return allFriends;
+};
 
-  const getFriends =  () => {
-    let allFriends =[]
-    db.collection("friends").get().then(function(querySnapshot) {
-  
-        querySnapshot.forEach(function(doc) {
-          let aFriend = doc.data();
-          aFriend.id=doc.id;
-          allFriends.push(aFriend)
-     
-        });
-  
-    })
-    return allFriends;
-  };
+const asyncGetFriends = async () => {
+  const { docs } = await db.collection('friends').get();
+  return docs.map((friend) => ({
+    ...friend.data(),
+    id: friend.id,
+  }));
+}
 
-  export { db, getFriends, addFriend };
+export { db, getFriends, addFriend, asyncGetFriends };
