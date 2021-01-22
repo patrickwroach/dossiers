21e9fb157ba577040092334f47666587e5552be1
proach720 <proach720@comcast.com>
2021-01-22 14:06:03 -0500

    freind list layout

--- src/components/Listing/index.js
+++ src/components/Listing/index.js
@@ -1,39 +1,39 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { asyncGetFriends, addFriend, deleteFriend } from '../../firebase/firebaseActions';
 
 const Listing = ({ friendData, setFriendData, }) => /**/ {
   const defaultState = {
-    firstName:"first-name",
-    lastName:"last-name"
+    firstName: "first-name",
+    lastName: "last-name"
   }
   const [newFriend, setNewFriend] = useState(defaultState);
-  
+
   const inputToState = (e) => {
     let toAddToState = newFriend;
     toAddToState[e.target.name] = e.target.value;
     setNewFriend(toAddToState);
   }
-  
+
   const refreshFriends = () => {
     (async () => {
       const friends = await asyncGetFriends();
-      setFriendData(friends);  
+      setFriendData(friends);
     })()
   }
-  
+
   const addFriendRefreshList = () => {
     if (newFriend.firstName && newFriend.lastName) {
-    addFriend(newFriend.firstName, newFriend.lastName);
-    const firstNameInput =  document.getElementById("first-name-input");
-    firstNameInput.value =  firstNameInput.placeholder
-    const lastNameInput =  document.getElementById("last-name-input");
-    lastNameInput.value =  lastNameInput.placeholder
-    setNewFriend({defaultState});
-    setTimeout(refreshFriends, 100)
+      addFriend(newFriend.firstName, newFriend.lastName);
+      const firstNameInput = document.getElementById("first-name-input");
+      firstNameInput.value = firstNameInput.placeholder
+      const lastNameInput = document.getElementById("last-name-input");
+      lastNameInput.value = lastNameInput.placeholder
+      setNewFriend({ defaultState });
+      setTimeout(refreshFriends, 100)
     } else {
       alert("Your friend needs a first and last name")
     }
-    
+
 
   };
 
@@ -42,34 +42,39 @@ const Listing = ({ friendData, setFriendData, }) => /**/ {
     setTimeout(refreshFriends, 100)
   }
 
-  
+
   const renderFriendsList = () => (
     friendData.map((friend, i) => (
-      <li key={friend.id}>
-        {friend.lastName}, {friend.firstName}
-        <button className="button material material-black" onClick={()=>{removeFriendRefreshList(friend.id)}}>X</button>
+      <li key={friend.id} className="listing_friend">
+        <div class="listing_friend-name">
+          {friend.lastName}, {friend.firstName}
+        </div>
+        <button className="button material material-black" onClick={() => { removeFriendRefreshList(friend.id) }}>X</button>
       </li>
     ))
   )
-  
 
-  
+
+
   return (
     <>
       <h1>Friends</h1>
-   
-      <h2>Add a Friend</h2>
-        <input type="text" id="first-name-input" name="firstName" placeholder="first name" onChange={(e)=>{inputToState(e)}} onFocus={(e)=>{e.target.value=""}}/>
-        <input type="text" id="last-name-input" name="lastName" placeholder="last name" onChange={(e)=>{inputToState(e)}} onFocus={(e)=>{e.target.value=""}} />
-        <button onClick={()=>{addFriendRefreshList()}}>Add 'em</button>
-  
-  
-      <h2>All Friends</h2>
-      <ul>
-        { renderFriendsList() }
+
+      <h2 class="listing_subheading">Add a Friend</h2>
+      <div class="listing_form">
+        <div class="listing_inputs-wrapper">
+          <input className="listing_input material" type="text" id="first-name-input" name="firstName" placeholder="first name" onChange={(e) => { inputToState(e) }} onFocus={(e) => { e.target.value = "" }} />
+          <input className="listing_input  material" type="text" id="last-name-input" name="lastName" placeholder="last name" onChange={(e) => { inputToState(e) }} onFocus={(e) => { e.target.value = "" }} />
+        </div>
+        <button className="button material material-black" onClick={() => { addFriendRefreshList() }}>Add 'em</button>
+      </div>
+
+      <h2 class="listing_subheading">All Friends</h2>
+      <ul class="listing_friends">
+        {renderFriendsList()}
       </ul>
     </>
   );
-  
+
 };
 export default Listing;
